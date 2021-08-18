ee2c003808b5b6d82035033183eba4f5910c7416
MLMridul <mdmridul344@gmail.com>
2021-08-18 14:17:37 +0600

    Github Add

---
+++ js/index.js
@@ -0,0 +1,51 @@
+
+
+function plus(){
+    var one_1= 10;
+    var one_2= 5;
+    var one_3=one_1 + one_2
+
+    document.getElementsByClassName('one')[0].innerHTML=one_3
+
+}
+
+
+function minus(){
+    var two_1= 100;
+    var two_2= 1;
+    var two_3=two_1 - two_2
+
+    document.getElementsByClassName('two')[0].innerHTML=two_3
+
+}
+
+
+function multiply(){
+    var three_1=25;
+    var three_2=4;
+    var three_3=three_1 * three_2
+
+    document.getElementsByClassName('three')[0].innerHTML=three_3
+
+}
+
+
+function divided(){
+    var four_1= 50;
+    var four_2= 5;
+    var four_3=four_1 / four_2
+
+    document.getElementsByClassName('four')[0].innerHTML=four_3
+
+}
+
+
+function percent(){
+    var five_1=100;
+    var five_2=70;
+    var five_3=five_1 % five_2
+
+    document.getElementsByClassName('five')[0].innerHTML=five_3
+
+}
+
